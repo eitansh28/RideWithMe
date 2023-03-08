37891425fbf57b0aae8ab88d8c6a1b3e28657c72
Matan-Shimon <matan874@gmail.com>
2023-03-08 14:11:26 +0200

    database connected

---
+++ RideWithMe/App.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import {View, TextInput, StyleSheet, Button} from "react-native";
+import { useState, useEffect } from "react";
+import firestore from "@react-native-firebase/firestore";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [user, setUser] = useState();
+
+  function create() {
+    firestore()
+      .collection("users")
+      .doc(`rdCiSL18n1uqgRQkig1X`)
+      .update({
+        FirstName: 'Matan',
+        LastName: 'Shimon',
+      });
+    alert("We got your data successfully :)");
+  }
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your mail"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter desired password"
+        value={pass}
+        onChangeText={setPass}
+      />
+      <Button title="Sign up" onPress={create}></Button>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  input: {
+    width: "90%",
+    fontSize: 15,
+    padding: 8,
+    borderColor: "gray",
+    borderWidth: 0.2,
+    borderRadius: 10,
+  },
+});
